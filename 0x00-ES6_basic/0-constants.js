72571055d04c2ee610108e084682674981350ea2
LoloDum <chinedumokamgba@gmail.com>
2023-04-17 17:38:33 -0700

    0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,5 +1,5 @@
 export function taskFirst() {
-	const task = 'I prefer const when I can.';
+  const task = 'I prefer const when I can.';
   return task;
 }
 
@@ -11,5 +11,5 @@ export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
 
-	return combination;
+  return combination;
 }
